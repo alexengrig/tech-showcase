70d938497a2e87656f5d319ed46fdef6e18fa8c7
Grig Alex <alexengrigdev@gmail.com>
2024-11-23 16:15:45 +0300

    Fix js-yaml types

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -14,19 +14,17 @@
  * limitations under the License.
  */
 
-// @ts-ignore
-import { readFileSync } from 'fs'
-// @ts-ignore
+import { readFileSync as loadFileContent } from 'fs'
 import { load as loadYaml } from 'js-yaml'
 
 import type { ShowcaseData } from '$lib/types'
 
 let data: ShowcaseData | null = null
 
-export const load = async(): Promise<ShowcaseData> => {
+export const load = async (): Promise<ShowcaseData> => {
   if (!data) {
     try {
-      const content = readFileSync('src/data/demo.yml')
+      const content = loadFileContent('src/data/demo.yml', 'utf8')
       data = loadYaml(content) as ShowcaseData
     } catch (error) {
       throw new Error('Failed to load data')
